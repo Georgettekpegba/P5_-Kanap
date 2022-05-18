bf4ed396968e134cd4531c111b88f239f88a26d9
KPEGBA <Kpegbageorgette@hotmail.com>
2022-05-18 19:25:44 +0200

    cartes js

--- front/js/script.js
+++ front/js/script.js
@@ -1,29 +1,35 @@
-const items = document.getElementById('items');
+const section = document.getElementById('items');
 fetch('http://127.0.0.1:3000/api/products/')
 .then ((response) => response.json())
 .then ((data)=> {
     for (let produit of data){
-        console.log(produit.name)
+        const productId = produit._id;
+        const ahref = document.createElement("a");
+        ahref.setAttribute("href", "./product.html?id=" +productId);
+        const article = document.createElement("article");
+        const img = document.createElement("img");
+        img.setAttribute("src", produit.imageUrl); 
+        img.setAttribute("alt", produit.altTxt);
+        const h3 = document.createElement("h3");
+        h3.classList.add("productName");
+        const p = document.createElement("p");
+        p.classList.add("productDescription");
+        const productName = produit.name;
+        const productDescription = produit.description;
+        h3.textContent = productName;
+        p.textContent = productDescription;
 
+    section.appendChild(ahref);
+    ahref.appendChild(article);
+    article.appendChild(img);
+    article.appendChild(h3);
+    article.appendChild(p);
     }
 });
 
-function Add (){
-    compteur += compteur;
-    console.log(compteur, "compteur àpres incrementation");
-    title.innerText = compteur;
-    image = document.getElementById
-    
-}
 
-function attache(id){
-    const attache = document.createElement("a")
-    attache.href = id
-    return attache
-}
 
-function createProductElement (data) {
 
-    const { a, article, img, h3, p } = createElementFactory('a', 'article', 'img', 'h3', 'p')
+
     
    
